b51c3c41dcd9f97f2412cba63334f45e17d54e51
MichalKrywult <218646090+MichalKrywult@users.noreply.github.com>
2026-09-03 14:21:36 +0200

    Refactor existing tests for stats

--- internal/stats/statistics_test.go
+++ internal/stats/statistics_test.go
@@ -2,52 +2,33 @@ package stats
 
 import (
 	"testing"
-	"time"
 
 	"github.com/MichalKrywult/mindgo/internal/domain"
-	"github.com/MichalKrywult/mindgo/internal/storage"
-	"github.com/MichalKrywult/mindgo/internal/tracker"
 )
 
 func TestCalculateAverageMood(t *testing.T) {
-	tracker, err := tracker.NewMoodTracker(&storage.MockStorage{})
-	if err != nil {
-		t.Fatalf("failed to create tracker: %v", err)
+	entries := []domain.MoodEntry{
+		{Mood: 2, Note: "Test"},
+		{Mood: 4, Note: "Test"},
 	}
 
-	entry1 := domain.MoodEntry{Mood: 2, Date: time.Now(), Note: "Test"}
-	time.Sleep(100 * time.Millisecond)
-	entry2 := domain.MoodEntry{Mood: 4, Date: time.Now(), Note: "Test"}
+	average, err := CalculateAverageMood(entries)
 
-	err = tracker.AddEntry(entry1)
 	if err != nil {
-		t.Fatalf("failed to add entry: %v", err)
-	}
-
-	err = tracker.AddEntry(entry2)
-	if err != nil {
-		t.Fatalf("failed to add entry: %v", err)
-	}
-
-	average, err := CalculateAverageMood(tracker.GetEntries())
-	if err != nil {
-		t.Errorf("unexpected error: %v", err)
+		t.Fatalf("unexpected error: %v", err)
 	}
 
 	if average != 3 {
-		t.Errorf("calculated average is wrong, expected: %v, got: %v", 3, average)
+		t.Errorf("expected average 3, got %v", average)
 	}
 }
 
 func TestCalculateAverageMoodWhenEmpty(t *testing.T) {
-	tracker, err := tracker.NewMoodTracker(&storage.MockStorage{})
-	if err != nil {
-		t.Fatalf("failed to create tracker: %v", err)
-	}
+	entries := []domain.MoodEntry{}
 
-	_, err = CalculateAverageMood(tracker.GetEntries())
+	_, err := CalculateAverageMood(entries)
 
 	if err == nil {
-		t.Error("expected an error for trying to calculate average on empty tracker ")
+		t.Fatal("expected error for empty entries")
 	}
 }
