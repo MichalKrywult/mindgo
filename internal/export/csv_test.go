b0de226eddc3f1285aaaef917ec18357fe3870d2
MichalKrywult <218646090+MichalKrywult@users.noreply.github.com>
2026-09-11 22:09:49 +0200

    Tests: add first unit tests (table-driven) to csv export file

---
+++ internal/export/csv_test.go
@@ -0,0 +1,49 @@
+package export
+
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/MichalKrywult/mindgo/internal/domain"
+)
+
+func TestExportCSV(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		entries  []domain.MoodEntry
+		expected string
+	}{
+		{
+			name: "correct line (single line)",
+			entries: []domain.MoodEntry{
+				{Date: time.Date(2026, 9, 7, 0, 0, 0, 0, time.UTC), Mood: 4, Note: "Test", ID: 1},
+			},
+			expected: "1,4,2026-09-07T00:00Z,Test\n",
+		},
+		{
+			name: "correct input (multiple lines)",
+			entries: []domain.MoodEntry{
+				{Date: time.Date(2026, 9, 7, 0, 0, 0, 0, time.UTC), Mood: 4, Note: "Test", ID: 1},
+				{Date: time.Date(2025, 1, 7, 0, 0, 0, 0, time.UTC), Mood: 8, Note: "Test2 ,", ID: 2},
+				{Date: time.Date(124, 1, 7, 0, 0, 0, 0, time.UTC), Mood: 2, Note: "Test3 space", ID: 3},
+			},
+			expected: "1,4,2026-09-07T00:00Z,Test\n" +
+				"2,8,2025-01-07T00:00Z,\"Test2 ,\"\n" +
+				"3,2,0124-01-07T00:00Z,Test3 space\n",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf bytes.Buffer // place in the RAM memory, that pretends to be a file
+			ExportCSV(tt.entries, &buf)
+			data := buf.String()
+			if data != tt.expected {
+				t.Errorf("expected=%v, got=%v", tt.expected, data)
+			}
+
+		})
+	}
+}
