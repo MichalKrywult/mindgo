abc01605ac04ce41db90c7dc357107d0a2462502
MichalKrywult <218646090+MichalKrywult@users.noreply.github.com>
2026-09-21 11:20:54 +0200

    Feat: add EnsureConfigFile function

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,10 +7,41 @@ import (
 )
 
 const (
+	defaultConfigFileName = "config.json"
 	defaultDataFileName   = "moods.json"
 	defaultExportFileName = "moods.csv"
 	appDirectoryName      = "mindgo"
+
+	defaultConfig = `{"configPath":"moods.json"}`
 )
+
+func EnsureConfigFile() error {
+	configPath, err := buildPath("", defaultConfigFileName)
+	if err != nil {
+		return fmt.Errorf("failed to get user config directory: %w", err)
+	}
+
+	_, err = os.ReadFile(configPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.MkdirAll(filepath.Dir(configPath), 0755)
+			if err != nil {
+				return fmt.Errorf("failed to create config directory: %w", err)
+			}
+
+			err = os.WriteFile(configPath, []byte(defaultConfig), 0644)
+			if err != nil {
+				return fmt.Errorf("failed to create config file: %w", err)
+			}
+			return nil
+		}
+
+		return fmt.Errorf("failed to read file %s: %w", configPath, err)
+	}
+
+	return nil
+}
+
 
 func buildPath(file, defaultFileName string) (string, error) {
 	configDir, err := os.UserConfigDir()
