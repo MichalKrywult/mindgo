4f32d715e052cdf7a0865ad1f1db6a3a71502560
MichalKrywult <218646090+MichalKrywult@users.noreply.github.com>
2026-09-21 21:19:26 +0200

    Feat: implement handling configFile to main; refactor BuildPath func from cli to SelectDataPath for clarity

--- main.go
+++ main.go
@@ -11,6 +11,24 @@ import (
 
 func main() {
 
+	configPath, err := config.BuildConfigPath()
+	if err != nil {
+		fmt.Println("Error with ConfigFile path:", err)
+		return
+	}
+
+	err = config.EnsureConfigFile(configPath)
+	if err != nil {
+		fmt.Println("Error with ConfigFile:", err)
+		return
+	}
+
+	conf, err := config.LoadConfigFromFile(configPath)
+	if err != nil {
+		fmt.Println("Error with loading from ConfigFile:", err)
+		return
+	}
+
 	parsedFlags, err := cli.ParseFlags()
 	if err != nil {
 		if err == flag.ErrHelp {
@@ -21,14 +39,13 @@ func main() {
 		return
 	}
 
-	dataPath, err := cli.BuildPath(parsedFlags)
+	dataPath, err := cli.SelectDataPath(parsedFlags, conf)
 	if err != nil {
 		fmt.Println("Error with path building:", err)
 		return
 	}
 
-	config := config.Config{DataPath: dataPath}
-	err = app.Run(config)
+	err = app.Run(config.Config{DataPath: dataPath})
 	if err != nil {
 		fmt.Println("Error starting apllication:", err)
 		return
